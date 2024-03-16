71fe4f0c155b44d11c4dd206720dab0f9e342531
ymejias <ymejias@cyberfuel.com>
2024-03-16 01:06:03 -0600

    Version 1.0
    - Actualizacion de test_sqlite.php

--- test/test_sqlite.php
+++ test/test_sqlite.php
@@ -1,43 +1,59 @@
 <?php
 // Require PHP 7.2+
-require __DIR__.'/../src/SQVar.php';
-require __DIR__.'/../src/SQLiteMan/funcList.php';
-require __DIR__.'/../src/SQLiteMan/Exception.php';
-require __DIR__.'/../src/SQLiteMan/Manager.php';
+require __DIR__.'/../src/_autoloader.php';
 
-$dsnLite='sqlite:'.__DIR__.'/test.db';
+$db=__DIR__.'/test.db';
 try{
-    $m=new \SQLiteMan\Manager(new PDO($dsnLite));
-    $m->conn()->setAttribute(PDO::ATTR_TIMEOUT, 5);
-    $m->conn()->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    //$m->dropTable('proc');
-    $r=$m->createTable('proc', [
-        'name'=>[
-            'type'=>'text',
+//    $m=new SQLiteManPDO(new PDO('sqlite:'.$db));
+//    $m->conn()->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $m=new SQLiteMan(new SQLite3($db));
+    $m->timeout(5);
+    $m->throwExceptions(true);
+    $m->exec($sql=$m->dropTable_sql('proc'));
+    $m->exec($sql=$m->createTable_sql('proc', [
+        'ID'=>[
+            'type'=>SQLiteMan::TYPE_INTEGER,
             'pk'=>1,
+            'ai'=>1,
             'notnull'=>true,
         ],
+        'name'=>[
+            'type'=>SQLiteMan::TYPE_TEXT,
+            'notnull'=>true,
+            'default'=>'',
+        ],
         'open'=>[
-            'type'=>'integer',
+            'type'=>SQLiteMan::TYPE_INTEGER,
             'default'=>0,
             'notnull'=>true,
         ],
         'close'=>[
-            'type'=>'integer',
+            'type'=>SQLiteMan::TYPE_INTEGER,
             'default'=>0,
             'notnull'=>true,
         ],
-        'a'=>[
-            'type'=>'integer',
-            'defaultExpr'=>$m->fn('datetime'),
+        'dt'=>[
+            'type'=>SQLiteMan::TYPE_INTEGER,
+            'defaultExpr'=>$m->fn_val('datetime', 'now', 'localtime'),
             'notnull'=>true,
         ],
     ], null, [
         'if'=>true,
-        'without_rowid'=>true
-    ]);
+    ]));
+    $i=-1;
+    while(++$i<256) $m->exec($sql=$m->insert_sql('proc', [
+        'name'=>'N:'.chr($i).'--',
+    ]));
+    $res=$m->query($sql=$m->select_sql(['*'], 'proc', [SQData::s("ID IN (1,5,23,230,99)")]));
+    foreach($res as $row){
+        echo json_encode($row).PHP_EOL;
+    }
 }catch(PDOException $err){
-    $e=\SQLiteMan\Exception::fromException($err);
+    $e=\SQLiteMan\Exception::fromPDOException($err);
+}catch(SQLite3Exception $err){
+    $e=\SQLiteMan\Exception::fromSQLiteException($err);
 }catch(Exception $err){
+    $e=$err;
 }
+
 exit;
