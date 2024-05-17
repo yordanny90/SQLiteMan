33bac1df5b682e4e9d82b518b4d6c9d6339f8b8c
ymejias <ymejias@cyberfuel.com>
2024-05-16 19:09:04 -0600

    Version 1.1
    - Limpiado de la librería de MySQL

--- src/MySQLMan.php
+++ src/MySQLMan.php
@@ -1,9 +1,30 @@
 <?php
 
-use MySQLMan\Functions;
-use MySQLMan\ManagerBase;
+use MySQLMan\Manager;
 
-class MySQLMan extends ManagerBase{
-    use Functions;
+class MySQLMan extends Manager{
+    protected $conn;
+
+    /**
+     * @param PDO $conn
+     * @throws Exception
+     */
+    public function __construct(PDO &$conn){
+        if($conn->getAttribute(PDO::ATTR_DRIVER_NAME)!=='mysql'){
+            throw new Exception('Invalid connection driver');
+        }
+        $this->conn=&$conn;
+    }
+
+    protected function quote(string $string){
+        return $this->conn->quote($string);
+    }
+
+    /**
+     * @return PDO
+     */
+    function conn(){
+        return $this->conn;
+    }
 
 }
---
+++ src/MySQLMan/Manager_adds.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MySQLMan;
+/**
+ * <b>IMPORTANTE:</b>
+ * <p>La funciones que se declaren aquí, deben apegarse a la documentación oficial de sqlite.</p>
+ * <br>
+ * Las funciones siempre se deben documentar siguiendo los estándares.
+ *
+ * El nombre de las funciones siempre debe iniciar con un guión bajo (_)
+ *
+ * Repositorio {@link https://github.com/yordanny90/SQLManager}
+ *
+ * @link https://dev.mysql.com/doc/refman/5.7/en/sql-function-reference.html
+ */
+trait Manager_adds{
+}
